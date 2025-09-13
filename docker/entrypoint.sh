a0b976247dc52bc6cb527f819df4632edcbbd337
Júnio César Rosa <juninhocapitolio@gmail.com>
2025-09-13 14:50:43 -0300

    tentando buildar

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 set -e
 
-# Instala dependências do Composer apenas se não existir vendor
 if [ ! -d "vendor" ] || [ -z "$(ls -A vendor)" ]; then
-  echo ">> Instalando dependências do Composer..."
-  composer install --no-interaction --prefer-dist --optimize-autoloader
+  echo ">> Instalando dependências do Composer no host..."
+  composer install --no-interaction --prefer-dist --optimize-autoloader --no-scripts
 fi
+
+echo ">> Executando scripts do Laravel..."
+composer run-script post-install-cmd --no-interaction || true
+composer run-script post-update-cmd --no-interaction || true
 
 exec "$@"
